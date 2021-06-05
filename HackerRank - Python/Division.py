9ed1fc3b158a0fe38e4a3e0e388c598cbd339606
Swapnil <81177774+Now-Tiger@users.noreply.github.com>
2021-06-06 03:45:10 +0530

    Create Division.py

---
+++ HackerRank - Python/Division.py
@@ -0,0 +1,16 @@
+
+a, b = int(input()), int(input())
+int_res = int(a / b)
+float_res = a // b
+print(int_res, float_res, sep = '\n')
+
+# ************************************* OR ***************************************************
+
+a = 4
+b = 3
+first = a / b
+second = a // b
+print(first)
+print(second)
+
+
