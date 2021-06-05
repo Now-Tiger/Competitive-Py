9ea873d0199120e3e70253fe67c39292c7c8a82a
Swapnil <81177774+Now-Tiger@users.noreply.github.com>
2021-06-06 03:33:56 +0530

    Create Arithmetic-Operators.py

---
+++ HackerRank - Python/Arithmetic-Operators.py
@@ -0,0 +1,28 @@
+
+x= int(input()) 
+y = int(input())
+print((a + b), (a - b), (a * b), sep='\n')
+
+##################################   OR   ##################################################
+
+a = 3
+y = 2 
+print(x + y)
+print(x - y)
+print(x * y)
+
+##################################   OR   ##################################################
+
+x = 3
+y = 2
+
+addition = x + y
+substraction = x - y
+multiplication = x * y
+
+print(addition)
+print(multiplication)
+print(substracton)
+
+
+
