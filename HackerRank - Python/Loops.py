2d698102231db511c80b4a78d90f4a2f1b103b16
Swapnil <81177774+Now-Tiger@users.noreply.github.com>
2021-06-06 03:54:04 +0530

    Create Loops.py

---
+++ HackerRank - Python/Loops.py
@@ -0,0 +1,15 @@
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        result = i * i
+        print(result)
+
+# **************************************** OR *****************************************
+        
+n = int(input())
+for i in range(n):
+  res = i**2 
+  print(res)
+   
+    
