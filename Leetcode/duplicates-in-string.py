9ec3c32311e9bef19f8b40b6d9e5a585ce0cf5a1
Swapnil <81177774+Now-Tiger@users.noreply.github.com>
2021-08-08 20:26:45 +0530

    easy

---
+++ Leetcode/duplicates-in-string.py
@@ -0,0 +1,23 @@
+# ----------------------------- Duplicates in a string -------------------------------- 
+# Write an efficient program to print all the duplicates and their counts in the input string .
+
+No_of_chars = 256
+
+def fillCharCounts(string, count) :
+    for i in string :
+        count[ord(i)] += 1
+    return count 
+
+def printDups(string) :
+    count = [0] * No_of_chars
+    count = fillCharCounts(string, count)
+    k = 0
+    for i in count :
+        if int(i) > 1 :
+            print(chr(k) + ", count = " + str(i))
+        k += 1
+
+string = "geeksforgeeks"
+print(printDups(string))
+
+
