c6917546d94dc411d8f61dc25780cdcb76ba4997
Swapnil <81177774+Now-Tiger@users.noreply.github.com>
2021-08-21 02:17:26 +0530

    easy

---
+++ Leetcode/missing-number.py
@@ -0,0 +1,17 @@
+
+
+# Problem : Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing 
+#           from the array.
+
+# Ex. 
+# Input: nums = [3,0,1]
+# Output: 2
+# Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums
+
+def missingNumber(nums) :
+        total_sum = sum(nums)
+        sum_idx = sum([idx[0] + 1 for idx in enumerate(nums)])
+        return sum_idx - total_sum
+
+nums = [0,1,3]
+print(missingNumber(nums))
