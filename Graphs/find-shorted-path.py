dc262483b15aa43a36ebeca341339b990f30ba37
Swapnil <81177774+Now-Tiger@users.noreply.github.com>
2021-09-01 02:21:57 +0530

    undirected-graph

---
+++ Graphs/find-shorted-path.py
@@ -0,0 +1,56 @@
+
+# ----------- Building an undirected graph and finding shortest path using Dictionaries in Python --------------
+
+def find_shorted_path(graph, start, goal):
+    explored = []
+    queue = [[start]]
+
+    if start == goal:
+        print("Same Node")
+        return
+     
+    # Loop to traverse the graph
+    # with the help of the queue
+    while queue:
+        path = queue.pop(0)
+        node = path[-1]
+         
+        # Condition to check if the
+        # current node is not visited
+        if node not in explored:
+            neighbours = graph[node]
+
+            for neighbour in neighbours:
+                new_path = list(path)
+                new_path.append(neighbour)
+                queue.append(new_path)
+                 
+                if neighbour == goal:
+                    print("Shortest path = ", *new_path)
+                    return
+            explored.append(node)
+ 
+    # Condition when the nodes
+    # are not connected
+    print("So sorry, but a connecting"\
+                "path doesn't exist :(")
+    return
+ 
+# Driver Code
+if __name__ == "__main__":
+    graph = {'A': ['B', 'E', 'C'],
+            'B': ['A', 'D', 'E'],
+            'C': ['A', 'F', 'G'],
+            'D': ['B', 'E'],
+            'E': ['A', 'B', 'D'],
+            'F': ['C'],
+            'G': ['C']}
+     
+    find_shorted_path(graph, 'D', 'G')
+
+# $ python find-shorted-path.py 
+# Shortest path =  D B A C G
+
+# T.c : O(nlogn)
+
+
