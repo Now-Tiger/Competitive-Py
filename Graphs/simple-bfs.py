db121b451e9bb1583293dddb39fb852d5f851943
Swapnil <81177774+Now-Tiger@users.noreply.github.com>
2021-09-05 03:25:48 +0530

    iterative

---
+++ Graphs/simple-bfs.py
@@ -0,0 +1,29 @@
+
+# -# ----------------------------------- Iterative Implementation of BFS --------------------------------------------- 
+
+from collections import deque
+
+def bfs(graph, root) :
+    visited, queue = set(), deque([root])
+    visited.add(root)
+
+    while queue :
+        s = queue.popleft()
+        print(s, end = '->')
+
+        for neighbour in graph[s] :
+            if neighbour not in visited :
+                visited.add(neighbour)
+                queue.append(neighbour)
+
+
+# Driver code :
+if __name__ == '__main__' :
+    graph = {0: [1, 2], 1: [2], 2: [3], 3: [1, 2]}
+    print(bfs(graph, 1))
+
+
+# $ python simple-bfs.py 
+# 1->2->3->None
+
+
