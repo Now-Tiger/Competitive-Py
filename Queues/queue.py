33ded371c6e414e9542ffc50bbeedb2597bb8bac
Swapnil <81177774+Now-Tiger@users.noreply.github.com>
2021-09-07 02:26:58 +0530

    simple-queue-implementation

---
+++ Queues/queue.py
@@ -0,0 +1,44 @@
+
+# ----------------------------- Queue implementation using array -----------------------------
+
+class Queue :
+    def __init__(self) :
+        self.items = []
+
+    def is_empty(self) :
+        return self.items == []
+    
+    # Add elemets into queue
+    def enqueue(self, data) :
+        self.items.append(data)
+
+    # Pop/remove elements from queue
+    def dequeue(self) :
+        return self.items.pop(0)
+
+    # Display queue 
+    def display(self) :
+        arr = []
+        for i in self.items :
+            arr.append(i)
+        return arr
+
+def main() :
+    queue = Queue()
+    queue.enqueue('google')
+    queue.enqueue('youtube')
+    queue.enqueue('udemy')
+    queue.enqueue('udacity')
+    queue.dequeue()
+    queue.dequeue()
+    print(queue.display())
+
+
+# Driver code
+if __name__ == '__main__' :
+    main()
+
+# $ python queue-implementation.py 
+# ['udemy', 'udacity']
+
+
