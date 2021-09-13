5601ecb4015cdcc78643aae1d699d4936c048f5a
Swapnil <81177774+Now-Tiger@users.noreply.github.com>
2021-09-14 00:25:19 +0530

    gfg

--- HackerRank - Python/find-missing.py
+++ HackerRank - Python/find-missing.py
@@ -4,7 +4,6 @@
 # You are given a list of n-1 integers and these integers are in the range of 1 to n. There are no duplicates in the list. 
 # One of the integers is missing in the list. Write an efficient code to find the missing integer.
 
-
 # Input:  { 3, 2, 4, 6, 1 }
 # Output: The missing element is 5
  
