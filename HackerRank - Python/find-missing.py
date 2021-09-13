3b3481e8f3421592a20c95ca7d7d1daf9887bcd3
Swapnil <81177774+Now-Tiger@users.noreply.github.com>
2021-09-14 00:24:51 +0530

    gfg

--- HackerRank - Python/find-missing.py
+++ HackerRank - Python/find-missing.py
@@ -20,15 +20,32 @@ def findmissing(arr) :
     total = sum(arr) 
     return (n + 1) + n * (n + 1) // 2 - total
 
-
 if __name__ == '__main__' :
     arr = [3, 4, 6, 1, 5]
     print(f'Missing value is : {findmissing(arr)}')
-
 
 # $ python find-missing.py 
 # Missing value is : 2
 
 # T.C = O(n),     S.C = O(1)
+
+# ------------------------------------------------------------------------------------------------------------------------------------
+
+# Above method can lead us to the overflow if the n is large, n order to avoid integer overflow, pick one number from known numbers and 
+# subtract one number from given numbers. This way there won’t have Integer Overflow ever.
+
+def getmissing(arr, n) :
+    i, total = 0, 1
+    for i in range(2, n + 2) :
+        total += i
+        total -= arr[i - 2]
+    return total
+
+if __name__ == '__main__' :
+    arr = [3, 2, 1, 4, 5, 7]
+    print(f'Missing value is : {getmissing(arr, len(arr))}')
+   
+# $ python find-missing.py 
+# Missing value is : 6
 
 
