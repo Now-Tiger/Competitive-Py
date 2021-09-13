f67036bac96f0da92f7815f29aa649558d2d7d27
Swapnil <81177774+Now-Tiger@users.noreply.github.com>
2021-09-14 00:31:06 +0530

    interpolation-search

--- Search/interpolation-search.py
+++ Search/interpolation-search.py
@@ -2,7 +2,7 @@
 Given a sorted array of n uniformly distributed values arr[], 
 write a function to search for a particular element x in the array. 
 
-intuatuion : The Interpolation Search is an improvement over Binary Search for instances, where the values 
+intuition : The Interpolation Search is an improvement over Binary Search for instances, where the values 
              in a sorted array are uniformly distributed 
 
 workflow :  interpolation search may go to different locations according to the value of the key being searched. 
@@ -12,22 +12,22 @@ workflow :  interpolation search may go to different locations according to the 
 formula : pos = lo + [ (x-arr[lo])*(hi-lo) / (arr[hi]-arr[Lo]) ]
 '''
 
-def interplotationSearch(arr, lo, hi, x):
+def interpolationSearch(arr, lo, hi, x):
     if(lo <= hi and x >= arr[lo] and x <= arr[hi]):
         pos = lo + ((hi - lo) // (arr[hi] - arr[lo])*(x - arr[lo]))
         if arr[pos] == x :
             return pos
         if arr[pos] < x :
-            return interplotationSearch(arr, pos+1, hi, x)
+            return interpolationSearch(arr, pos+1, hi, x)
         if arr[pos] > x :
-            return interplotationSearch(arr, lo, pos-1, x)
+            return interpolationSearch(arr, lo, pos-1, x)
     return -1
 
 # Driver code to check : 
 arr = [10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30]
 n = len(arr)
 x = 22
-index = interplotationSearch(arr, 0, n-1, x)
+index = interpolationSearch(arr, 0, n-1, x)
 
 if index != -1 :
     print('Element found at index ', index)
