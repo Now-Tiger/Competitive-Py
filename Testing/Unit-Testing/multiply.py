b927d554639c61e793c39462c86a1a7384d0ad24
Swapnil <81177774+Now-Tiger@users.noreply.github.com>
2022-04-20 04:48:39 +0530

    Unit-testing

---
+++ Testing/Unit-Testing/multiply.py
@@ -0,0 +1,39 @@
+#!/usr/bin/ python3
+import math
+import numpy as np
+
+
+def multiply_with_loop(first: int, second: int) -> int:
+    return sum(first for _ in range(second))
+
+
+def better_multiply(first: int, second: int) -> int:
+    return sum(abs(first) for _ in range(abs(second)))
+
+
+def numpy_multiply(first: int, second: int) -> int:
+    return np.multiply(first, second)
+
+
+def simple_mult(first: int, second: int) -> int:
+    return first * second
+
+
+if __name__ == "__main__":
+    print(
+        multiply_with_loop(-17, 3),
+        multiply_with_loop(-17, 0),
+        multiply_with_loop(-17, -19),
+        "\n-----------\n",
+        better_multiply(-17, 3),
+        better_multiply(-17, 0),
+        better_multiply(-17, -19),
+        "\n-----------\n",
+        simple_mult(-17, 3),
+        simple_mult(-17, 0),
+        simple_mult(-17, -19),
+        "\n-----------\n",
+        numpy_multiply(-17, 3),
+        numpy_multiply(-17, 0),
+        numpy_multiply(-17, -19),
+    )
---
+++ Testing/Unit-Testing/multiplytest.py
@@ -0,0 +1,27 @@
+#!/usr/bin/ python3
+import multiply
+import unittest
+
+
+class TestMultiply(unittest.TestCase):
+    def test_with_two_positives(self):
+        self.assertEqual(multiply.numpy_multiply(17, 19), 17 * 19)
+        self.assertEqual(multiply.numpy_multiply(1, 2), 2)
+
+    def test_with_one_zero(self):
+        self.assertEqual(multiply.numpy_multiply(17, 0), 0)
+        self.assertEqual(multiply.numpy_multiply(0, 17), 0)
+
+    def test_with_zeroes(self):
+        self.assertEqual(multiply.numpy_multiply(0, 0), 0)
+
+    def test_with_one_negative(self):
+        self.assertEqual(multiply.numpy_multiply(17, -19), 17 * (-19))
+        self.assertEqual(multiply.numpy_multiply(-19, 17), (-19) * 17)
+
+    def test_with_two_negatives(self):
+        self.assertEqual(multiply.numpy_multiply(-17, -19), 17 * 19)
+
+
+if __name__ == "__main__":
+    unittest.main()
