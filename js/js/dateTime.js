6a8fd8c63a1e09dfb1e4f295326ab88111c0c9c0
Swapnil <81177774+Now-Tiger@users.noreply.github.com>
2023-05-29 21:52:18 +0530

    .

--- js/js/dateTime.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env/ node
-
-exports.myDate = function() {
-    return Date();
-}
--- js/js/httpHelloWorld.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-var http = require('http')
-
-http.createServer(function(request, result) {
-    result.write("Hello client");
-    result.end();
-}).listen(8080);
-
